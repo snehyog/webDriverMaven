4d86886a5e2ae447259eb35f8d49d7e287ac2229
Yogesh Wakchaure <Yogesh Wakchaure>
2015-12-22 17:21:38 -0500

    Initial commit

---
+++ src/main/java/com/w2a/webdriverMaven/App.java
@@ -0,0 +1,13 @@
+package com.w2a.webdriverMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/w2a/webdriverMaven/LoginTest.java
@@ -0,0 +1,33 @@
+package com.w2a.webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() throws InterruptedException{
+		
+		driver=new FirefoxDriver();
+//		driver.wait(3000L);
+		
+	}
+	@Test
+	public void doLogin() throws InterruptedException
+	{
+		driver.get("https://www.google.com");
+		driver.findElement(By.id("gb_70")).click();
+	}
+//	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+}
